f33cc24231ce63ed153b99a7959365f4b326bd0b
honjow <honjow311@gmail.com>
2024-03-09 19:20:42 +0800

    feat: use global decky-frontend-lib

--- rollup.config.js
+++ rollup.config.js
@@ -24,12 +24,13 @@ export default defineConfig({
     })
   ],
   context: 'window',
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'decky-frontend-lib'],
   output: {
     file: 'dist/index.js',
     globals: {
       react: 'SP_REACT',
       'react-dom': 'SP_REACTDOM',
+      'decky-frontend-lib': 'DFL',
     },
     format: 'iife',
     exports: 'default',
