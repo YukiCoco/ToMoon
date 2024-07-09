e9f63bf8f36f8f29ae458a78d967beefae57c617
Kurisu Sayo <i@kuri.su>
2024-07-09 23:05:24 +0800

    fix: wrong display in narrow screen.

--- tomoon-web/src/App.jsx
+++ tomoon-web/src/App.jsx
@@ -13,17 +13,17 @@ function App() {
   }
 
   return (
-    <div style={{ background: '#FAFFE3' }} className='flex justify-center item-center'>
+    <div style={{ background: '#FAFFE3', minHeight: '100vh' }} className='flex justify-center items-center'>
       <div className='container grid grid-cols-12'>
         <div className='col-span-12 lg:col-start-2 lg:col-span-10'>
-          <div className='flex justify-center items-center h-screen flex-col gap-4'>
+          <div className='flex justify-center items-center min-h-screen flex-col gap-4'>
             <h1 className="text-6xl tomoon-title">
               Tomoon
             </h1>
             <div className='w-full flex px-2'>
               <input id="input-url" className='tomoon-input grow lg:text-2xl text-xl pl-5' type="text" placeholder='Clash 订阅链接' value={url} onChange={handleUrlChange} />
               <button className='tomoon-button text-4xl flex justify-center items-center' onClick={() => { on_download_btn_click(url) }}>
-                <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="tomoon-svg" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M0.0228571 48L48 24L0.0228571 0L0 18.6667L34.2857 24L0 29.3333L0.0228571 48Z" fill="white" />
                 </svg>
               </button>
